d540a87113083ff1a28f16e479a503dbbb7f0248
Akash Unagar <akashvu49@gmail.com>
2025-05-08 22:34:41 +0530

    Add files via upload

---
+++ 8.3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n, num;
+    cout << "Enter the number of transaction IDs: ";
+    cin >> n;
+
+    set<int> uniqueIDs;
+
+    cout << "Enter the transaction IDs:\n";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> num;
+        uniqueIDs.insert(num);
+    }
+
+    cout << "\nUnique Transaction IDs (sorted):\n";
+    for (int id : uniqueIDs)
+    {
+        cout << id << " ";
+    }
+    cout << endl;
+
+    cout << "\nName : Akash Unagar\nID : 24CE130";
+
+    return 0;
+}
