859070cf74142fa38f172e4c87fe66e22241fe3e
Akash Unagar <akashvu49@gmail.com>
2025-05-08 17:54:03 +0530

    Add files via upload

---
+++ 5.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+public:
+    int Real_part, Imgnary_part;
+    void input()
+    {
+        cout << "Enter num :";
+        cin >> Real_part >> Imgnary_part;
+    }
+    void operator+(Complex &b)
+    {
+        Complex Ans;
+        Ans.Real_part = Real_part + b.Real_part;
+        Ans.Imgnary_part = Imgnary_part + b.Imgnary_part;
+        cout << Ans.Real_part << "+" << Ans.Imgnary_part << "i";
+    }
+};
+int main()
+{
+    Complex a, b, x;
+    a.input();
+    b.input();
+    a + b;
+    cout << "\nName : Akash Unagar\nID : 24CE130";
+    return 0;
+}
